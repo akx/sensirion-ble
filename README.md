efcb78cf6bbde3fc726ec261ab5e5ebb87392cdf
Aarni Koskela <akx@iki.fi>
2023-06-01 14:39:41 +0300

    Add untested support for data formats 4 and 6
    Refs https://github.com/home-assistant/core/issues/93678

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Parser for Sensirion BLE devices.
 
 * [MyCO2 (aka SCD4x CO2 Gadget)](https://sensirion.com/products/catalog/SCD4x-CO2-Gadget/)
 
+## Untested devices
+
+* SHT3x based gadgets
+* SHT4x based gadgets
+
 Other devices could be supported at a later time, but I don't have any at hand.
 
 ## Reference
--- src/sensirion_ble/converters.py
+++ src/sensirion_ble/converters.py
@@ -6,8 +6,43 @@ from sensor_state_data import SensorDeviceClass, Units
 
 from sensirion_ble.types import ConversionResult
 
+CO2_PPM = (SensorDeviceClass.CO2, Units.CONCENTRATION_PARTS_PER_MILLION)
+RH_PERCENTAGE = (SensorDeviceClass.HUMIDITY, Units.PERCENTAGE)
+TEMP_CELSIUS = (SensorDeviceClass.TEMPERATURE, Units.TEMP_CELSIUS)
+
+
+def _convert_type_4(raw_data: bytes) -> ConversionResult:
+    # SHT3x gadgets. UNTESTED.
+    temp_ticks, humidity_ticks = struct.unpack("<hh", raw_data[4:8])
+    # Conversion:
+    # T = - 45 + ((175.0 * ticks) / (2^16 - 1))
+    # RH = (100.0 * ticks) / (2^16 - 1)
+    temp = -45 + ((175.0 * temp_ticks) / (2**16 - 1))
+    humidity = (100.0 * humidity_ticks) / (2**16 - 1)
+    data = {
+        TEMP_CELSIUS: round(temp, 1),
+        RH_PERCENTAGE: round(humidity, 1),
+    }
+    return ConversionResult(raw_data[2:4].hex().upper(), data)
+
+
+def _convert_type_6(raw_data: bytes) -> ConversionResult:
+    # SHT4x gadgets. UNTESTED.
+    temp_ticks, humidity_ticks = struct.unpack("<hh", raw_data[4:8])
+    # Conversion:
+    # T = - 45 + ((175.0 * ticks) / (2^16 - 1))
+    # RH = -6 + (125.0 * ticks) / (2^16 - 1)
+    temp = -45 + ((175.0 * temp_ticks) / (2**16 - 1))
+    humidity = -6 + (125.0 * humidity_ticks) / (2**16 - 1)
+    data = {
+        TEMP_CELSIUS: round(temp, 2),
+        RH_PERCENTAGE: round(humidity, 2),
+    }
+    return ConversionResult(raw_data[2:4].hex().upper(), data)
+
 
 def _convert_type_8(raw_data: bytes) -> ConversionResult:
+    # Sensirion MyCO2.
     temp_ticks, humidity_ticks, co2 = struct.unpack("<hhh", raw_data[4:10])
     # Conversion:
     # T = - 45 + ((175.0 * ticks) / (2^16 - 1))
@@ -19,13 +54,15 @@ def _convert_type_8(raw_data: bytes) -> ConversionResult:
         # The datasheet for the SCD4x sensor states that the accuracy
         # of the temperature sensor is ±0.8°C, so one digit after the
         # decimal point should be enough.
-        (SensorDeviceClass.TEMPERATURE, Units.TEMP_CELSIUS): round(temp, 1),
-        (SensorDeviceClass.HUMIDITY, Units.PERCENTAGE): round(humidity, 1),
-        (SensorDeviceClass.CO2, Units.CONCENTRATION_PARTS_PER_MILLION): co2,
+        TEMP_CELSIUS: round(temp, 1),
+        RH_PERCENTAGE: round(humidity, 1),
+        CO2_PPM: co2,
     }
     return ConversionResult(raw_data[2:4].hex().upper(), data)
 
 
 CONVERTERS = {
+    b"\x00\x04": _convert_type_4,
+    b"\x00\x06": _convert_type_6,
     b"\x00\x08": _convert_type_8,
 }
